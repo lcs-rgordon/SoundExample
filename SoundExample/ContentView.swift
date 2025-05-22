47a9356e7c7c226836eeb5017a8259b18b09b749
Russell Gordon <rgordon@lcs.on.ca>
2025-05-21 20:35:41 -0400

    Initial Commit

---
+++ SoundExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SoundExample
+//
+//  Created by Russell Gordon on 2025-05-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SoundExample/SoundExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundExampleApp.swift
+//  SoundExample
+//
+//  Created by Russell Gordon on 2025-05-21.
+//
+
+import SwiftUI
+
+@main
+struct SoundExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
